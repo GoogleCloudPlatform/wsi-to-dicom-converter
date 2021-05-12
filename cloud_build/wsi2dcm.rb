061eafc81da89d543341aa91a7c4e57fafbe07b0
Daniel Beaudreau <dbeaudreau@google.com>
2021-05-12 17:13:13 +0000

    update boost dependency link

--- cloud_build/wsi2dcm.rb
+++ cloud_build/wsi2dcm.rb
@@ -18,7 +18,7 @@ class Wsi2dcm < Formula
       args << "-Dpkg_config_libdir=#{lib} -DCMAKE_C_COMPILER=gcc-9 -DCMAKE_CXX_COMPILER=g++-9 " << ".."
       system "wget", "https://dicom.offis.de/download/dcmtk/dcmtk362/dcmtk-3.6.2.zip"
       system "unzip", "dcmtk-3.6.2.zip"	
-      system "wget", "https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz"
+      system "wget", "https://boostorg.jfrog.io/artifactory/main/release/1.69.0/source/boost_1_69_0.tar.gz"
       system "tar", "xvzf", "boost_1_69_0.tar.gz"     
       system "wget", "https://github.com/open-source-parsers/jsoncpp/archive/0.10.7.zip"
       system "unzip", "0.10.7.zip"     
