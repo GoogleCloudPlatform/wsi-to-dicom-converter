f90c1e6636bf2a3d2c92bf57b3d079d4c7174933
Philbrick <Kenneth.Philbrick@gmail.com>
2021-09-21 13:08:25 -0500

    Add Progressive Downsampling

--- endToEnd/endToEndTest.sh
+++ endToEnd/endToEndTest.sh
@@ -32,22 +32,22 @@ apt-get install wget dcmtk -y
 
 fileName=./tests/CMU-1-Small-Region.svs
 
-#test - generate jpeg2000 DICOM, read by dcmtk and check with expected tags
+#test - use nearest neighbor downsampling,  generate jpeg2000 DICOM, read by dcmtk and check with expected tags
 ./build/wsi2dcm $fileName ./endToEnd/ --seriesDescription test1 --levels 9 --startOn 8 --compression jpeg2000
 dcm2json ./endToEnd/level-8-frames-0-1.dcm ./endToEnd/test1GeneratedTags.json
 compare ./endToEnd/test1GeneratedTags.json ./endToEnd/test1ExpectedTags.json
 
-#test - generate jpeg DICOM, read by dcmtk and check with expected tags
+#test - use nearest neighbor downsampling, generate jpeg DICOM, read by dcmtk and check with expected tags
 ./build/wsi2dcm $fileName ./endToEnd/ --seriesDescription test2 --stopOn 1
 dcm2json ./endToEnd/level-0-frames-0-30.dcm ./endToEnd/test2GeneratedTags.json
 compare ./endToEnd/test2GeneratedTags.json ./endToEnd/test2ExpectedTags.json
 
-#test - generate DICOM without compression and compare image with expected one
+#test - use nearest neighbor downsampling, generate DICOM without compression and compare image with expected one
 ./build/wsi2dcm $fileName ./endToEnd/ --seriesDescription test3  --levels 6 --startOn 5 --tileHeight 100  --tileWidth 100 --compression raw
 dcm2pnm ./endToEnd/level-5-frames-0-1.dcm ./endToEnd/test3GeneratedImage.ppm
 diff ./endToEnd/test3GeneratedImage.ppm ./endToEnd/test3ExpectedImage.ppm
 
-#test - generate DICOM with dropped first row and column and compare image with expected one
+#test - use nearest neighbor downsampling, generate DICOM with dropped first row and column and compare image with expected one
 ./build/wsi2dcm $fileName ./endToEnd/ --seriesDescription test4  --levels 6 --startOn 5 --tileHeight 100  --tileWidth 100 --compression raw --dropFirstRowAndColumn
 dcm2pnm ./endToEnd/level-5-frames-0-1.dcm ./endToEnd/test4GeneratedImage.ppm
 diff ./endToEnd/test4GeneratedImage.ppm ./endToEnd/test4ExpectedImage.ppm
@@ -62,3 +62,13 @@ rm ./endToEnd/level-5-frames-0-1.dcm
 ./build/wsi2dcm $fileName ./endToEnd/ --seriesDescription test6  --levels 6 --startOn 5 --tileHeight 100  --tileWidth 100 --compression raw --dropFirstRowAndColumn --bilinearDownsampling
 dcm2pnm ./endToEnd/level-5-frames-0-1.dcm ./endToEnd/test6GeneratedImage.ppm
 diff ./endToEnd/test6GeneratedImage.ppm ./endToEnd/test6ExpectedImage.ppm
+
+#test - use progressiveDowsampling & bilinear downsampling to generate DICOM
+./build/wsi2dcm $fileName ./endToEnd/ --seriesDescription test7  --levels 6 --tileHeight 100  --tileWidth 100 --compression raw --dropFirstRowAndColumn --bilinearDownsampling --progressiveDownsample
+dcm2pnm ./endToEnd/level-5-frames-0-1.dcm ./endToEnd/test7GeneratedImage.ppm
+diff ./endToEnd/test7GeneratedImage.ppm ./endToEnd/test7ExpectedImage.ppm
+
+#test - use progressiveDowsampling & nearest neighbor downsampling to generate DICOM
+./build/wsi2dcm $fileName ./endToEnd/ --seriesDescription test8  --levels 6 --tileHeight 100  --tileWidth 100 --compression raw --dropFirstRowAndColumn --progressiveDownsample
+dcm2pnm ./endToEnd/level-5-frames-0-1.dcm ./endToEnd/test8GeneratedImage.ppm
+diff ./endToEnd/test8GeneratedImage.ppm ./endToEnd/test8ExpectedImage.ppm
