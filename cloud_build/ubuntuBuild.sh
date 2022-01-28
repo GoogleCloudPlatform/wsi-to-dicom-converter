3dd2bd1ff03933845726006484d3ba7c7454e83a
Philbrick <Kenneth.Philbrick@gmail.com>
2022-01-28 10:21:09 -0600

    Update ubuntuBuild.sh

--- cloud_build/ubuntuBuild.sh
+++ cloud_build/ubuntuBuild.sh
@@ -32,6 +32,7 @@ wget -O /libjpeg_turbo.zip https://github.com/libjpeg-turbo/libjpeg-turbo/archiv
 unzip /libjpeg_turbo.zip
 mkdir -p /libjpeg-turbo-2.1.2/build
 cd /libjpeg-turbo-2.1.2/build
+ls ..
 cmake -G"Unix Makefiles" ..
 make -j12
 make install
