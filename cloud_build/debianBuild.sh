ec1966375938ee98641c2bb1143908ccd6c9dfab
Philbrick <Kenneth.Philbrick@gmail.com>
2024-01-15 12:12:09 -0600

    Update libraries.

--- cloud_build/debianBuild.sh
+++ cloud_build/debianBuild.sh
@@ -47,17 +47,17 @@ apt-get install -y \
 #2
 # installing in /workspace
 apt-get install -y nasm
-wget -O libjpeg_turbo.zip https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/3.0.0.zip > /dev/null
+wget -O libjpeg_turbo.zip https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/3.0.1.zip > /dev/null
 unzip libjpeg_turbo.zip > /dev/null
 rm libjpeg_turbo.zip
-mkdir -p ./libjpeg-turbo-3.0.0/build
-cd ./libjpeg-turbo-3.0.0/build
+mkdir -p ./libjpeg-turbo-3.0.1/build
+cd ./libjpeg-turbo-3.0.1/build
 cmake -G"Unix Makefiles" ..
 make -j12
 make install
 cd ..
 cd ..
-rm -rf libjpeg-turbo-3.0.0
+rm -rf libjpeg-turbo-3.0.1
 #3
 apt-get install -y liblcms2-dev libzstd-dev libwebp-dev
 wget -O v2.5.0.zip  https://github.com/uclouvain/openjpeg/archive/v2.5.0.zip > /dev/null
@@ -72,17 +72,17 @@ cd ..
 cd ..
 rm -rf openjpeg-2.5.0
 #4
-wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/4.8.0.zip > /dev/null
+wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/4.9.0.zip > /dev/null
 unzip opencv.zip  > /dev/null
 rm opencv.zip
-mkdir -p ./opencv-4.8.0/build
-cd ./opencv-4.8.0/build
+mkdir -p ./opencv-4.9.0/build
+cd ./opencv-4.9.0/build
 cmake .. -DBUILD_LIST=core,imgproc,imgcodecs
 make -j12
 make install
 cd ..
 cd ..
-rm -rf opencv-4.8.0
+rm -rf opencv-4.9.0
 #5
 wget -O abseil.zip https://github.com/abseil/abseil-cpp/archive/refs/tags/20230802.1.zip > /dev/null
 unzip abseil.zip > /dev/null
@@ -95,29 +95,29 @@ cd ..
 cd ..
 rm -rf abseil-cpp-20230802.1
 #6
-wget -O dcmtk-3.6.7.zip https://github.com/DCMTK/dcmtk/archive/refs/tags/DCMTK-3.6.7.zip > /dev/null
-unzip dcmtk-3.6.7.zip > /dev/null
-rm dcmtk-3.6.7.zip
-mkdir -p ./dcmtk-DCMTK-3.6.7/build
-cd ./dcmtk-DCMTK-3.6.7/build
-cmake -DDCMTK_FORCE_FPIC_ON_UNIX:BOOL=TRUE -DDCMTK_ENABLE_CXX11:BOOL=TRUE -DDCMTK_ENABLE_CHARSET_CONVERSION:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=ON -DDCMTK_MODULES:STRING="ofstd;oflog;dcmdata;dcmimgle;dcmimage;dcmjpeg" ..
+wget -O dcmtk-3.6.8.zip https://github.com/DCMTK/dcmtk/archive/refs/tags/DCMTK-3.6.8.zip > /dev/null
+unzip dcmtk-3.6.8.zip > /dev/null
+rm dcmtk-3.6.8.zip
+mkdir -p ./dcmtk-DCMTK-3.6.8/build
+cd ./dcmtk-DCMTK-3.6.8/build
+cmake -DDCMTK_FORCE_FPIC_ON_UNIX:BOOL=TRUE -DDCMTK_ENABLE_CXX11:BOOL=TRUE -DDCMTK_ENABLE_CHARSET_CONVERSION:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=ON -DDCMTK_MODULES:STRING="oficonv;ofstd;oflog;dcmdata;dcmimgle;dcmimage;dcmjpeg" ..
 make -j12
 make DESTDIR=/ install
 cd ..
 cd ..
-rm -rf dcmtk-DCMTK-3.6.7
-export DCMDICTPATH=/usr/local/share/dcmtk/dicom.dic
+rm -rf dcmtk-DCMTK-3.6.8
+export DCMDICTPATH=/usr/local/share/dcmtk-3.6.8/dicom.dic
 export PATH=/usr/local/bin:$PATH
 # 7
-wget -O boost_1_83_0.tar.gz https://boostorg.jfrog.io/artifactory/main/release/1.83.0/source/boost_1_83_0.tar.gz > /dev/null
-tar xvzf boost_1_83_0.tar.gz > /dev/null
-rm boost_1_83_0.tar.gz
-cd boost_1_83_0
+wget -O boost_1_84_0.tar.gz https://boostorg.jfrog.io/artifactory/main/release/1.84.0/source/boost_1_84_0.tar.gz > /dev/null
+tar xvzf boost_1_84_0.tar.gz > /dev/null
+rm boost_1_84_0.tar.gz
+cd boost_1_84_0
 ./bootstrap.sh --prefix=/usr/ --with-libraries=system,atomic,thread,chrono,program_options,log,filesystem > /dev/null
 ./b2
 ./b2 install > /dev/null
 cd ..
-rm -rf boost_1_83_0
+rm -rf boost_1_84_0
 # 8
 apt-get update
 apt-get install -y python3-full pip libjpeg-dev ninja-build
--- endToEnd/strip_pixel_data.py
+++ endToEnd/strip_pixel_data.py
@@ -5,5 +5,6 @@ import pydicom
 if __name__ == '__main__':
   ds = pydicom.dcmread(sys.argv[1])
   del ds['PixelData']
+  ds.file_meta.TransferSyntaxUID = '1.2.840.10008.1.2.1'
   ds.save_as(ds.filename)
 
